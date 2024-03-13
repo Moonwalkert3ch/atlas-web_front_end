e248a2694a7038955c41978849bad477ef0a3fd7
Moonwalkert3ch <6620@holbertonstudents.com>
2024-03-12 20:05:27 -0500

    complete task 3

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,17 @@
+// closures and loops
+
+function createClassRoom(numbersOfStudents) {
+    function studentSeat(seat) {
+        return function() {
+            return seat;
+        }
+    }
+
+    let students = [];
+    
+    for(i = 0; i < numbersOfStudents; i++) {
+        students[i] = studentSeat(i + 1)();
+    }
+    return students;
+}
+let classRoom = createClassRoom(10);
