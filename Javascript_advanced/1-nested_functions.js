d46c1b1ea6974dcfcf68a32d1c668fb8a9adc16a
Moonwalkert3ch <6620@holbertonstudents.com>
2024-03-12 19:29:03 -0500

    task 1

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,25 @@
+// closure scope chain
+
+let globalVariable = "Welcome";
+
+function outer() {
+    alert(globalVariable);
+
+    let course = "Holberton";
+
+    function inner()  {
+        alert(globalVariable + " " + course);
+
+        let exclamation = "!";
+
+        function inception() {
+            alert(globalVariable + " " + course + exclamation);
+        }
+
+        inception();
+    }
+
+    inner();
+}
+
+outer();
